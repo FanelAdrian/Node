107a25228155625c2f925078a9710255e45e5d94
Fanel Adrian <faneladrianpop@gmail.com>
2022-11-11 15:44:37 +0100

    dia1

--- proyecto_node/index.js
+++ proyecto_node/index.js
@@ -1,13 +1,9 @@
+
+
+
 const { readConsole } = require("./readConsole.js");
 const { writeAndRead } = require("./writeAndReadObject.js");
 
-
-// const callAllFunction = (path, obj, callback) => {
-//     readConsole(callback);
-//     writeAndRead(path, obj);
-// }
-
-// callAllFunction("persona.json", {}, console.log);
 
 readConsole(function (persona){
 
