bfdc30a3c7c42338b6ed33c79abdc37b0feb09ae
Fanel Adrian <faneladrianpop@gmail.com>
2022-11-14 13:53:03 +0100

    dia

--- node_2/src/routers/Professional.routers.js
+++ node_2/src/routers/Professional.routers.js
@@ -2,10 +2,10 @@ const {Router} = require ("express")
 const router = Router();
 const usersCtrl = require("../controller/Professional.controller")
 router.get("/", usersCtrl.getStart);
-// router.get("/professional", usersCtrl.getUser);
-// router.get("/professional/:id", usersCtrl.getUserParams);
-// router.post("/professional", usersCtrl.postUser);
-// router.put("/professional", usersCtrl.putUser);
-// router.delete("/professional", usersCtrl.deleteUser);
+router.get("/professional", usersCtrl.getUser);
+router.get("/professional/:id", usersCtrl.getUserParams);
+router.post("/professional", usersCtrl.postUser);
+router.put("/professional", usersCtrl.putUser);
+router.delete("/professional", usersCtrl.deleteUser);
 
 module.exports = router;
