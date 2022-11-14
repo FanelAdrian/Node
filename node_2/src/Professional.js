e05ee51c618e761d1fba83143fe2f9d13e5dfc00
Fanel Adrian <faneladrianpop@gmail.com>
2022-11-14 13:48:21 +0100

    dia

---
+++ node_2/src/Professional.js
@@ -0,0 +1,21 @@
+class Professional {
+    constructor(name, age, weight, height,) {
+        this.name = name;
+        this.age = age;
+        this.weight = weight;
+        this.height = height;
+
+
+
+
+    }
+    information() {
+        console.log(this.name);
+        console.log(this.age);
+        console.log(this.weight);
+        console.log(this.height);
+    }
+
+}
+
+module.exports = { Professional };
---
+++ node_2/src/routers/Professional.routers.js
@@ -0,0 +1,11 @@
+const {Router} = require ("express")
+const router = Router();
+const usersCtrl = require("../controller/Professional.controller")
+router.get("/", usersCtrl.getStart);
+// router.get("/professional", usersCtrl.getUser);
+// router.get("/professional/:id", usersCtrl.getUserParams);
+// router.post("/professional", usersCtrl.postUser);
+// router.put("/professional", usersCtrl.putUser);
+// router.delete("/professional", usersCtrl.deleteUser);
+
+module.exports = router;
